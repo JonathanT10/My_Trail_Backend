d0c383d4264dc3bd8e8b999bd3ef8c41a4fa247a
Justin Adams <khisanth05@gmail.com>
2021-05-27 11:19:55 -0400

    Minor- aligned the wall styling

--- frontend/src/components/post.jsx
+++ frontend/src/components/post.jsx
@@ -1,43 +1,64 @@
-import {useState} from 'react';
-import Img from './subComponents/img';
-import LD from './subComponents/LD';
-import Location from './subComponents/location';
-import Mood from './subComponents/mood';
-import Profile from './subComponents/profile';
-import TextBody from './subComponents/textBody';
-import Reply from './subComponents/reply';
-import Rating from './subComponents/rating';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import { useState } from "react";
+import Img from "./subComponents/img";
+import LD from "./subComponents/LD";
+import Location from "./subComponents/location";
+import Mood from "./subComponents/mood";
+import Profile from "./subComponents/profile";
+import TextBody from "./subComponents/textBody";
+import Reply from "./subComponents/reply";
+import Rating from "./subComponents/rating";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
-const Wall = (props)=>{
-    const [text, setText] = useState('');
+const Wall = (props) => {
+  const [text, setText] = useState("");
 
-    const handleChange = (event) => {
-        setText(event.target.value);
-      };
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
 
-    const handleClick =()=>{
-        const newComment={
-        }
-        props.addNewComment(newComment);
-        setText('');
-    }
+  const handleClick = () => {
+    const newComment = {};
+    props.addNewComment(newComment);
+    setText("");
+  };
 
-    return(
-        <Container fluid>                
-                <Img />
-                <LD />
+  return (
+    <Container fluid>
+      <Row>
+        <Col sm={8}>
+          <Row>
+            <TextBody />
+          </Row>
+          <Row>
+            <Col sm={4}>
+              <LD />
+            </Col>
+            <Col sm={8}>
+              <Row>
                 <Location />
-                <Mood />
-                <Profile />
-                <TextBody />
-                <Reply />
+              </Row>
+              <Row>
                 <Rating />
-        </Container>
-    )
-}
+                <Mood />
+              </Row>
+              <Row>
+                <Reply />
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+        <Col sm={2}>
+          <Img />
+        </Col>
+        <Col sm={2}>
+          <Profile />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Wall;
