5710179476350b1d41f0544bd5fb5fc154149cf8
Justin Adams <khisanth05@gmail.com>
2021-05-27 20:13:48 -0400

    Major- added login page

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,24 +1,43 @@
 import {useState} from 'react';
-import Post from './post';
-import NewPost from './newPost';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
 import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './css/wall.css';
+import axios from 'axios';
+import { useParams } from 'react-router';
 
 const Wall = (props)=>{
-    const [text, setText] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleChange = (event) => {
-        setText(event.target.value);
-      };
+    const emailChange = (event) => {
+        setEmail(event.target.value);
+    };
+    const passwordChange = (event) => {
+        setPassword(event.target.value);
+    };
 
-    const handleClick =()=>{
-        const newComment={
-        }
-        props.addNewComment(newComment);
-        setText('');
+    const handleClick = async (email, password) =>{
+        axios
+            .post(`http://localhost:5000/api/auth/`, {
+                params:{email: email, password: password}
+                })
+            .then(response => {
+                const { token } = response.data;
+                localStorage.setItem('token', token);
+                console.log(token);
+            })
     }
+
+
+    // useEffect(() => {
+    //     axios
+    //       .get(`http://localhost:5000/api/ytclone/${videoId}`)
+    //       .then((response) => setComments(response.data));
+    //       console.log(videoId)
+    //   }, [videoId]);
 
     // post needs to map all posts matching logged in user, and everyone on friends list
 
@@ -26,7 +45,23 @@ const Wall = (props)=>{
         <Container fluid>
             <Table>                    
                 <Row className="postStyle">
-                    Login page here
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" onChange={emailChange}/>
+                            <Form.Text className="text">
+                            -----We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" onChange={passwordChange}/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={()=>handleClick(email, password)}>
+                            Submit
+                        </Button>
+                        </Form>
                 </Row>
             </Table>
         </Container>
