08c69f601e23172b8fbc8b003624ecd4a01cac51
Justin Adams <khisanth05@gmail.com>
2021-06-02 22:48:58 -0500

    Minor - BUG - Page does not refresh correctly after logging in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,37 @@
-import { useState } from "react";
-import "./App.css";
-import axios from "axios";
+// import { useState } from "react";
+import React from 'react';
+import './components/css/wall.css';
 import Wall from './components/wall';
 import Login from './components/login';
 import Register from './components/register';
 import ErrorPage from './components/errorpage';
-import jwtDecode from 'jwt-decode';
+// import jwtDecode from 'jwt-decode';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from "./components/profile";
 
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  const [user, setUser] = useState();
-  const jwt = localStorage.getItem('token');
+  // const [user, setUser] = useState();
+  // const jwt = localStorage.getItem('token');
+  
+              
+
+  // const authUser = async()=>{
+  //   console.log("auth trigger test")
+  //   await axios
+  //     .get(`http://localhost:5000/api/user/${user._id}`)
+  //     .then((response) => setUser(response.data))
+  // }
    
     return(
       <div className="background">
         <Switch>
-          <Route path="/" exact component={Login}  render={props => {
-              setUser(jwtDecode(jwt))
-              if (!user) {
-                return <Register />;
-              } else { 
-                axios
-                  .get(`http://localhost:5000/api/user/${user._id}`)
-                  .then((response) => setUser(response.data))
-                return <Wall props={user}/>
-              }
-            }}/>
-          <Route path="/login" component={Login} />
-          <Route path="/wall" component={Wall}/>
+          <Route path="/" component={Login} />
+          <Route path="/wall" component={Wall} />
           <Route path="/register" component={Register} />
           <Route path="/error" component={ErrorPage} />
-          <Route path="/profile" component={Profile} props={user}/>
+          <Route path="/profile" component={Profile} />
         </Switch>
       </div>
   );
--- frontend/src/components/wall.jsx
+++ frontend/src/components/wall.jsx
@@ -4,8 +4,22 @@ import NewPost from './newPost';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import './css/wall.css';
+// import { useState } from "react";
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
-const Wall = (props)=>{
+const Wall = () =>{
+    const jwt = localStorage.getItem('token');
+    const userObject = jwtDecode(jwt);
+
+
+    const authUser = ()=>{
+        console.log("auth trigger test")
+        const user = axios.get(`http://localhost:5000/api/user/${userObject._id}`, {headers: {Authorization : 'Bearer' + jwt}})
+        return user;
+    }
+
+    const user = authUser();
     // const [text, setText] = useState('');
 
     // const handleChange = (event) => {
@@ -20,16 +34,16 @@ const Wall = (props)=>{
     // }
 
     // post needs to map all posts matching logged in user, and everyone on friends list
-    console.log(props._id)
+    
 
     return(
         <Container fluid>
             profile link here              
                 <Row className="postStyle">
-                    <Post props={props}/>
+                    <Post props={user}/>
                 </Row>
                 <Row className="newPostStyle">
-                    <NewPost props={props} />
+                    <NewPost props={user} />
                 </Row>
         </Container>
     )
