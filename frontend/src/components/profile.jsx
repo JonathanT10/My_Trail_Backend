0481cb3f3e9ac86834d8b45d65617275574ca347
Justin Adams <khisanth05@gmail.com>
2021-06-05 16:14:44 -0500

    Minor - Added selected file box to profile page when adding a profile image

--- frontend/src/components/profile.jsx
+++ frontend/src/components/profile.jsx
@@ -14,6 +14,8 @@ const Profile = (props)=>{
     const jwt = localStorage.getItem('token');
     const userObject = jwtDecode(jwt);
     const [user, setUser] = useState();
+	const [selectedFile, setSelectedFile] = useState();
+	const [isSelected, setIsSelected] = useState(false);
 
     const authUser = async (userObject, jwt)=>{
         const user = await axios.get(`http://localhost:5000/api/user/${userObject._id}`, {headers: {Authorization : 'Bearer' + jwt}});
@@ -37,6 +39,20 @@ const Profile = (props)=>{
         window.location = '/wall';
     };
 
+    const imgChange = (event) => {
+		setSelectedFile(event.target.files[0]);
+		setIsSelected(true);
+	};
+
+	const handleSubmission = async () => {
+        axios
+            .put(`http://localhost:5000/api/user/uploadmulter/${user._id}`, {headers: {Authorization : 'Bearer' + jwt}}, {file: selectedFile})
+            .then(response => {
+                console.log(response);
+                // window.location = '/profile';
+            });
+	};
+
     return(
         <>
         {user ?
@@ -55,7 +71,27 @@ const Profile = (props)=>{
                     <Row className="profileContent"> 
                         <Col>
                         <div>
-                            <img src={user.img} alt="" />
+                            <img src={`../../public/uploads/${user.img}`} alt="" />
+                            <br/>
+                            <div>
+                                <input type="file" name="img" onChange={imgChange} />
+                                {isSelected ? (
+                                    <div className="loginText">
+                                        <p>Filename: {selectedFile.name}</p>
+                                        <p>Filetype: {selectedFile.type}</p>
+                                        <p>Size in bytes: {selectedFile.size}</p>
+                                        <p>
+                                            lastModifiedDate:{' '}
+                                            {selectedFile.lastModifiedDate.toLocaleDateString()}
+                                        </p>
+                                    </div>
+                                ) : (
+                                    <p className="loginText">Select a file to show details</p>
+                                )}
+                                <div className="loginText">
+                                <Button className="btn btn-success btn-md" onClick={() => handleSubmission()}>Submit</Button>
+                                </div>
+                            </div>
                         </div>
                         </Col>  
                         <Col>            
